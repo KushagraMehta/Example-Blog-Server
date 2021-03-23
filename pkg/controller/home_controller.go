102bd78dfa4bf1400bbc475a05aa5ac99a6adf8d
Kushagra Mehta <kushagramehta2000@gmail.com>
2021-03-23 22:28:39 +0530

    Adding DB check in home file

--- pkg/controller/home_controller.go
+++ pkg/controller/home_controller.go
@@ -1,12 +1,20 @@
 package controller
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/KushagraMehta/Example-Blog-Server/pkg/responses"
 )
 
 func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
+	var greeting string
+	err := server.DB.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
 
-	responses.JSON(w, http.StatusOK, "We are Up & running")
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	responses.JSON(w, http.StatusOK, fmt.Sprintf("We are Up and running..!!\n%s", greeting))
 }
